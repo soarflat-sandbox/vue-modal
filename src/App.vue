3a9c6e23d2f8e0a6443ce880ccc825b7b6578c1c
soarflat <soarflat777@gmail.com>
2018-02-05 20:11:45 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <button
+      type="button"
+      class="btn"
+      @click="showModal"
+    >
+      Open Modal!
+    </button>
+
+    <modal
+      v-show="isModalVisible"
+      @close="closeModal"
+    />
+  </div>
+</template>
+
+<script>
+  import modal from './components/modal.vue';
+
+  export default {
+    name: 'app',
+
+    components: {
+      modal
+    },
+
+    data() {
+      return {
+        isModalVisible: false,
+      };
+    },
+
+    methods: {
+      showModal() {
+        this.isModalVisible = true;
+      },
+      closeModal() {
+        this.isModalVisible = false;
+      },
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
